32776f78481aa8f7e4fcc641cf0df2ec0a99a357
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-08-27 23:15:37 +0200

    import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+
 // Change code below this line
 
 console.log(galleryItems);
